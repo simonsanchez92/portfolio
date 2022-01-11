18eef8b24b8cc1a819c9bfeae58e67b7bfdc75b4
Simon Sanchez <simonsanchez.1992@gmail.com>
2022-01-11 19:17:03 -0300

    translating

--- public/script.js
+++ public/script.js
@@ -1,19 +1,16 @@
+const navToggler = document.getElementById("nav-toggler");
+const nav = document.getElementById("nav");
 
-const navToggler = document.getElementById('nav-toggler');
-const nav = document.getElementById('nav');
-
-
-
-navToggler.addEventListener('click', ()=>{
-    nav.classList.toggle('hide');
-    // nav.style.transform = 'translateX(270px)';
-    navToggler.classList.toggle('show');
+navToggler.addEventListener("click", () => {
+  nav.classList.toggle("hide");
+  // nav.style.transform = 'translateX(270px)';
+  navToggler.classList.toggle("show");
 });
 
-window.addEventListener('resize', ()=>{
-    let w = window.innerWidth;
-   
-    if(w < 1000){
-    nav.classList.add('hide');
-    }
-})
+window.addEventListener("resize", () => {
+  let w = window.innerWidth;
+
+  if (w < 1000) {
+    nav.classList.add("hide");
+  }
+});
